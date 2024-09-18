8c8ad8a70ff2bbd5dc0452561ac740ea214dc904
unknown <theo.denys@student.uclouvain.be>
2024-09-18 19:21:11 +0200

    Version finale

--- devoir20sep.py
+++ devoir20sep.py
@@ -1,63 +1,57 @@
 
-#SP1bis) Fixer direction pour Y
+#SP1 et 1bis) Fixer l'orientation pour faire allere vers la coordonée X ou Y de VOLDEMORT
 
 def reorientation_north() :
     while get_direction!=NORTH :
-        if get_direction=WEST
+        if get_direction==WEST
             turn_right()
-        elif get_direction=EAST
+        elif get_direction==EAST
             turn_left()
         else 
             turn_right()
 def reorientation_south() :
     while get_direction!=SOUTH :
-        if get_direction=WEST
+        if get_direction==WEST
             turn_left()
-        elif get_direction=EAST
+        elif get_direction==EAST
             turn_right()
         else 
             turn_right()
             turn_right()
             
-
-def reorientation_east ():
+def reorientation_east():
     while get_direction!=EAST:
-        if get_direction=WEST:
-            turn_right(2)
-        elif get_direction!=NORTH:
+        if get_direction==WEST:
+            turn_right()
+            turn_right()
+        elif get_direction != NORTH:
             turn_right()
         else:
             turn_left()
-def reorientation_west () :
-    while get_direction!=WEST:
-        if get_direction=EAST:
-            turn_right(2)
-        elif get_direction!=NORTH:
+def reorientation_west() :
+    while get_direction != WEST:
+        if get_direction==EAST:
+            turn_right()
+            turn_right()
+        elif get_direction != NORTH:
             turn_left()
         else:
             turn_right()
-#SUD    
-#On fait en sorte que tout le temps il soit face au Nord pour standardiser
-    
+#######
 #SP2 bis
-
 
 while get_target_x != get_x
     #while is_in_front_of_enemy
     while can_move:
     #Il faut mettre NORD ou SUD
         if get_target_x>get_x():
-            reorientation_east
+            reorientation_east()
             move()
         elif get_target_x < get_x:
-            reorientation_west
+            reorientation_west()
             move()
-    
-    #elif get_target_x==get_x
-        #reorientation_north
-
         #SP3 bis
-    while can_move == false 
+    while can_move==false 
         if get_target_x < get_x:
             turn_left()
             move()
@@ -68,56 +62,33 @@ while get_target_x != get_x
             turn_left()
        
  print ("Harry is facing Voldemort")
+########
 
-#SP1) Fixer direction pour X
-
-#On fait en sorte que tout le temps il soit face orienté vers l'est pour standardiser
-
-
-
-
-#SP3 countornement des obstacles pour les Y
-
-
-    elif get_target_y == get_y
-        print ("Harry is facing Voldemort")
-
-while get_target_y != get_y
-    #SP2 vers une des coordonees de voldemort (Y ou X)
+#SP2 vers une des coordonees de voldemort (Y ou X)
 #On fait les Y
+#SP3 countornement des obstacles pour les Y
+while get_target_y != get_y
+
     while can_move:
     #Il faut mettre NORD ou SUD
         if get_target_y>get_y():
            turn_right()
         elif get_target_x > get_x:
             turn_right()
-           reorientation_south
+           reorientation_south()
             move()
         elif get_target_y < get_y:
-            reorientation_north
+            reorientation_north()
             move()
-    while can_move == false:
+    while can_move==false:
         if get_target_y < get_y:
             turn_left()
             move()
             turn_right()
         elif get_target_y > get_y:
             turn_right()
-                move()
-            turn_left   ()
-
+            move()
+            turn_left()
 
 if get_target_y==get_y :
     print ("Strike !")
-
-
-
-
-
-#        turn_left()
-#       move()
-#        turn_right()
-#    else:
-#        turn_right()
-#        move()
-#        turn_left   ()
