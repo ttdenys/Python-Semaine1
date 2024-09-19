3e926a08dad38e4641f67656087cd5a422d22580
Sean De Winter <seandwm@gmail.com>
2024-09-19 17:47:22 +0200

    Optimiser

--- #get to target x from east.py
+++ #get to target x from east.py
@@ -10,30 +10,33 @@ def set_orientation_west() :
             turn_right()
 if get_direction!=west:
     set_orientation_west()
-while can_move==true and not is_in_front_of_enemy:
-    move 
-    while get_x!=get_target_x:
+while get_x!=get_target_x:
+    while can_move==true and not is_in_front_of_enemy:
+        move 
         set_orientation_west
-     if not is_in_front_of_enemy:
-         move
-        #on a resolu le sp1
-while not can_move:
-    turn_right
-    if not is_in_front_of_enemy:
-        move 
+        if not is_in_front_of_enemy:
+            if can_move == true :
+                move
+            #on a resolu le sp1
     while not can_move:
-        turn right
-    if can_move:
-        move
-    turn_left
-    #sp2 fini
-while is_in_front_of_enemy:
-    turn_right
-    if not is_in_front_of_enemy:
-        move 
-    else:
         turn_right
-    turn_left
+        if not is_in_front_of_enemy:
+            if can_move == true :
+                move 
+        while not can_move:
+            turn_right
+        if can_move: 
+            move
+        turn_left
+        #sp2 fini
+    while is_in_front_of_enemy:
+        turn_right
+        if not is_in_front_of_enemy:
+            if can_move == true :
+                move 
+        else:
+            turn_right
+        turn_left
 #SP3 FIN
 if get_direction!=west:
     set_orientation_west()
