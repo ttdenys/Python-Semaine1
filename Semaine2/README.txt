5d48fd3a1baadda4d5e0ace7ecbf54ca4e9a46d7
ttdenys <theo.denys@student.uclouvain.be>
2024-09-24 10:08:42 +0200

    Version finale

---
+++ Semaine2/README.txt
@@ -0,0 +1,6 @@
+On doit faire 3 colones, la première avec les entiers de 1 à 10, la deuxième avec les carrés de ces entiers, et la troisième avec la somme de ces carrés.
+On défini sum_of_square à 0 au début, car au début c'est forcément 0. J'ai appris que l'interpréteur Python ne vas pas attribuer d'office une valeur "null" à une variable non attribuée.
+Il faut donc bien définir les variables au débuts!
+Pour avoir des colones, j'ai tout mis dans le même print, en séparant chque élément par un tab. Ma méthode diverge un peu de celle de l'énoncée, on on devait utiliser la formule
+i(i+1)*(2*i-1)/6, mais je trouve ma méthode plus "propre".
+
--- Semaine2/mise_en_route.py
+++ Semaine2/mise_en_route.py
@@ -1,31 +1,4 @@
-#Mission 1 #https://syllabus-interactif.info.ucl.ac.be/syllabus/info1-exercises/PART_I/MISSION_1/mission
-#Dans cette mission initiale, vous allez produire une table des carrés et des sommes des carrés :
-#Ecrivez un programme qui affiche à l'écran les nombres, les carrés et la somme des carrés pour les nombres entiers de 1 à 10, comme dans l'exemple ci-dessus. Ecrivez un programme qui affiche à l'écran les nombres, les carrés et la somme des carrés pour les nombres entiers de 1 à 10.
-#Utilisez la fonction print(), une boucle while et une variable pour calculer la somme au fur et à mesure.
-
-#SP1 : Carrés de 1 à 10
-#SP1A : Range 1-10, SP1B : Carré de chaque nombre
-#SP2 : Somme de ces carrés
-#Sp3  faires des colones
-
-
-#Sp1a
-first_10_numbers = range (1,11,1)
-#while x<=10 :
-  #  (x**2)
-  #first_10_numbers_squared_range =
-range (first_10_numbers (1, 100, i**2))
-for i in first_10_numbers:
-    print(i)
-print ("\t")
-def first_10_numbers_squared ():
-    for i in first_10_numbers:
-        square=i**2
-        first_10_numbers_squared_range.append(square)
-    print(square)
-print (first_10_numbers_squared_range)
-#first_10_numbers_squared()= first_10_numbers_squared_n
-#range (first_10_numbers_squared)
-#for i in first_10_numbers_squared_range:
- #   sum_square=i(i+1)*(2*i-1)/6
-  #  print (sum_square)
+sum_of_square=0
+for i in range (1,10+1):
+    sum_of_square = sum_of_square + i**2
+    print (i ,"\t", i**2,"\t", sum_of_square)
