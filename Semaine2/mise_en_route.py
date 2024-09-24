4bdb480452de83c9a732e04ced77b54deb620b6e
ttdenys <theo.denys@student.uclouvain.be>
2024-09-24 15:06:10 +0200

    Hotfix

--- Semaine2/mise_en_route.py
+++ Semaine2/mise_en_route.py
@@ -1,4 +1,4 @@
 sum_of_square=0
 for i in range (1,10+1):
     sum_of_square = sum_of_square + i**2
-    print (i ,"\t", i**2,"\t", i**2,"\t", sum_of_square)
+    print (i ,"\t", i**2,"\t",sum_of_square, "\t",sum_of_square)
